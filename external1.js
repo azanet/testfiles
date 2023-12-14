b6043572b7fef7ee5744564a78931e92a1cdb74f
ⵣΨ <azan3t@gmail.com>
2023-12-14 12:01:50 +0100

    Update external1.js

--- external1.js
+++ external1.js
@@ -1,1 +1,9 @@
-alert(0);
+// archivo.js
+
+// Función que se ejecutará al cargar la página
+function saludar() {
+  console.log("¡Hola desde archivo.js! La página se ha cargado.");
+}
+
+// Llamada a la función cuando se carga la ventana
+window.onload = saludar;
